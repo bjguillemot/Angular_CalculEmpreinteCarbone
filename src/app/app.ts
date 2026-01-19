0648741f365a06165a5674a734c3f2afd2f66b07
bjguillemot <130311394+bjguillemot@users.noreply.github.com>
2026-01-19 14:33:20 +0100

    Creating components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
+import { CarbonFootprint } from "./carbon-footprint/carbon-footprint";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Footer, CarbonFootprint],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/carbon-footprint/carbon-footprint.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carbon-footprint',
+  imports: [],
+  templateUrl: './carbon-footprint.html',
+  styleUrl: './carbon-footprint.scss',
+})
+export class CarbonFootprint {
+
+}
