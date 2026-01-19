d83d7add966aa7fc47c58ef02d75737d5982803e
bjguillemot <130311394+bjguillemot@users.noreply.github.com>
2026-01-19 15:50:07 +0100

    Move to methods

--- src/app/carbon-footprint/carbon-footprint.ts
+++ src/app/carbon-footprint/carbon-footprint.ts
@@ -11,8 +11,8 @@ import { DecimalPipe } from '@angular/common';
 })
 export class CarbonFootprint {
   
-  protected distanceKm: number;
-  protected consumptionPer100Km: number;
+  protected distanceKm!: number;
+  protected consumptionPer100Km!: number;
   protected travels: Array<{ distanceKm: number, consumptionPer100Km: number }>;
 
   constructor(){
@@ -24,13 +24,20 @@ export class CarbonFootprint {
       { distanceKm: 450, consumptionPer100Km: 9 }
     ];
 
-    this.distanceKm = this.travels.reduce((acc, val) => {
-      return acc + val.distanceKm;
-    }, 0);
+    this.calculateDistanceKm();
+    this.calculateConsumptionPer100Km();
+  }
 
+  private calculateConsumptionPer100Km() {
     this.consumptionPer100Km = this.travels.reduce((acc, val) => {
       return acc + val.consumptionPer100Km;
     }, 0) / this.travels.length;
+  }
+
+  private calculateDistanceKm() {
+    this.distanceKm = this.travels.reduce((acc, val) => {
+      return acc + val.distanceKm;
+    }, 0);
   }
   
   
@@ -52,6 +59,8 @@ export class CarbonFootprint {
     const distanceKm = this.randomIntFromInterval(50, 450);
     const consumptionPer100Km = this.randomIntFromInterval(5, 9);
     this.travels.push({ distanceKm, consumptionPer100Km })
+    this.calculateDistanceKm();
+    this.calculateConsumptionPer100Km();
   }
 
   private randomIntFromInterval(min: number, max: number) { // min and max included 
