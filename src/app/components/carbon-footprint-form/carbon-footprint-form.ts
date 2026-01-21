0eebf14dbbd23bde7c211b40d16d83c0aa87f2bc
bjguillemot <130311394+bjguillemot@users.noreply.github.com>
2026-01-21 11:51:40 +0100

    Niveau 2 - travel form

--- src/app/components/carbon-footprint-form/carbon-footprint-form.ts
+++ src/app/components/carbon-footprint-form/carbon-footprint-form.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { CarbonFootprintCompute } from '../../services/carbon-footprint-compute';
+import { Travel } from '../../models/travel';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-carbon-footprint-form',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './carbon-footprint-form.html',
   styleUrl: './carbon-footprint-form.scss',
 })
 export class CarbonFootprintForm {
+
+  protected travelForm: FormGroup;
+
+  constructor(private readonly cfc: CarbonFootprintCompute){
+    this.travelForm = new FormGroup({
+      distance: new FormControl(null, [Validators.required, Validators.min(0)]),
+      consumption: new FormControl(null, [Validators.required, Validators.min(0)]),
+      date: new FormControl(null, [Validators.required])
+    })
+  }
+
+  onClickSubmit() {
+    if(this.travelForm.valid){
+      const distanceKm = this.travelForm.get('distance')?.value;
+      const consumptionPer100Km = this.travelForm.get('consumption')?.value;
+      const date = this.travelForm.get('date');
+      this.cfc.addTravel({ distanceKm, consumptionPer100Km })
+    }
+  }
 
 }
