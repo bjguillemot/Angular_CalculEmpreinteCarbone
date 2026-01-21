d72abd4baec2b88f3fa56d3100ec011a67f52e87
bjguillemot <130311394+bjguillemot@users.noreply.github.com>
2026-01-21 11:33:08 +0100

    Niveau 1 - login form

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,29 +1,49 @@
 import { Component } from '@angular/core';
 import { Router } from "@angular/router";
 import { User } from '../../services/user';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   protected showLogoutBtn: boolean;
+  protected username: string;
+  protected password: string;
+  protected hasErrorLogin: boolean;
 
   constructor(private readonly authService: User, private readonly router: Router){
     this.showLogoutBtn = authService.isLogged();
+    this.username = '';
+    this.password = '';
+    this.hasErrorLogin = false;
   }
 
   goToSummary(){
     this.router.navigateByUrl('/summary')
-    this.authService.login("Jean");
   }
 
-  logout(){
-    if(this.authService.isLogged()){
+  onClickLogout() {
+    if (this.authService.isLogged()) {
       this.authService.logout();
       this.showLogoutBtn = false;
-    } 
+    }
+  }
+
+  onClickLogin() {
+    const isUsernameValid: boolean = this.username.length > 3;
+    const isPasswordValid: boolean = this.password.length > 6;
+    if (isPasswordValid && isUsernameValid) {
+      this.hasErrorLogin = false;
+      if (!this.authService.isLogged()) {
+      this.authService.login(this.username);
+      this.showLogoutBtn = true;
+      }
+    } else {
+      this.hasErrorLogin = true;
+    }
   }
 }
